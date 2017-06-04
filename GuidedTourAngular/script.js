2007ffad18f5a04a5c81970512ff2979eddfca7e
JaredSelling <jstephen2014@fau.edu>
2017-06-04 11:35:00 -0400

    Add to do list app in AngularJS

--- GuidedTourAngular/script.js
+++ GuidedTourAngular/script.js
@@ -5,15 +5,31 @@ appX.controller('appCtrl', function($scope) {
     
     $scope.tasks = [];
     
+    var taskData = localStorage['tasksList'];
+    
+    if(taskData !== undefined) {
+        $scope.tasks = JSON.parse(taskData);
+    }
+    
    $scope.searchEnter = function() {
        if(event.which == 13 && $scope.task != undefined) {
            $scope.addTask();
        }
-       return false;
    }; 
     
     $scope.addTask = function() {
-        $scope.tasks.push($scope.task);
+        $scope.tasks.push({'taskMessage':$scope.task, 'status': false});
         console.log($scope.tasks);
+        $scope.task = '';
     };
+    
+    $scope.contentEdit = function() {
+        event.target.contentEditable = event.target.contentEditable == "false" ? "true" : "false";
+    }
+    
+    $scope.saveEdit = function(msg) {
+        if(event.which == 13 && msg != undefined) {
+           $scope.contentEdit();
+        }
+    }
 });
