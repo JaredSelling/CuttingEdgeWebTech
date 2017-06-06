4d9575d25b4221daff9d46bcddaa5f1ba65cf8b4
Jared Selling <jstephen2014@fau.edu>
2017-06-06 06:07:05 -0400

    Add unordered list in Angular

--- Project2/script.js
+++ Project2/script.js
@@ -1,5 +1,10 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('todoCtrl', function($scope) {
+   $scope.totalTodos = 4;
     
-})
+    $scope.todos = [
+        {text: 'Learn Angular', done: false},
+        {text: 'Build an Angular app', done: false}
+    ];
+});
