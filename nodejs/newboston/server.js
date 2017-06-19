099dd54c9da3a49ca676bd94669f65454d946482
Jared Selling <jstephen2014@fau.edu>
2017-06-19 05:28:56 -0400

    Set up for express.js

--- nodejs/newboston/server.js
+++ nodejs/newboston/server.js
@@ -1,24 +1,18 @@
+var connect = require('connect');
 var http = require('http');
-var fs = require('fs');
 
-//404 response
-function send404Response(response) {
-    response.writeHead(404, {'Content-Type': 'text/plain'});
-    response.write("Error 404: Page not found!");
-    response.end();
-};
+var app = connect();
 
-//handle user request
-function onRequest(request, response) {
-    
-    if(request.method == 'GET' && request.url == '/') {
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream('./index.html').pipe(response);
-    }else{
-        send404Response(response);
-    }
-    
-};
+function profile(request, response) {
+    console.log('User requested profile');
+}
 
-http.createServer(onRequest).listen(8080);
-console.log('server running');
+function forum(request, response) {
+    console.log('User requested forum');
+}
+
+app.use('/profile', profile);
+app.use('/forum', forum);
+
+http.createServer(app).listen(8080);
+console.log('server running...');
